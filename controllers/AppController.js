fe6aadfabe762a48131f132b3cc087f8ea50967c
Tony <nya.tony2010@gmail.com>
2023-12-05 19:54:23 +0100

    feat(server.js, routes/index.js, controllers/AppController.js): First API

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  // Endpoint definition: GET /status
+  static getStatus(request, response) {
+    // Check if Redis and the DB are alive
+    // and return the response with a status code 200
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  // Endpoint definition: GET /stats
+  static async getStats(request, response) {
+    // Get the number of users in the DB
+    const users = await dbClient.nbUsers();
+    // Get the number of files in the DB
+    const files = await dbClient.nbFiles();
+    // Return with a status code 200
+    response.status(200).json({ users: users, files: files });
+  }
+}
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,15 @@
+import { express } from 'express';
+import { router } from './routes/index';
+
+const PORT = parseInt(process.env.PORT, 10) || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
